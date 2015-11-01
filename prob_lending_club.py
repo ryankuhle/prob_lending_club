4f68a52db44e24b6e0485401b40ed2807b9cb316
Ryan Kuhle <ryan@ryankuhle.com>
2015-11-01 10:51:09 -0600

    Read and clean data of NULL values

--- prob_lending_club.py
+++ prob_lending_club.py
@@ -2,9 +2,10 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import scipy.stats as stats
 
+#Read and clean data
+loansData = pd.read_csv('https://spark-public.s3.amazonaws.com/dataanalysis/loansData.csv')
+loansData.dropna(inplace=True)
 
-#Read in data
-#Clean it
 #Load to pandas
 #Create box plot, histogram, QQ for column Amount.Requested
 #compare results with Amount.Funded.By.Investors
